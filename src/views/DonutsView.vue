a7d90183d6490d349f1a7ca5ec2279c65f3eef8b
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-03-10 17:17:43 +0200

    Update endpoint

--- src/views/DonutsView.vue
+++ src/views/DonutsView.vue
@@ -45,7 +45,7 @@ export default {
   },
   methods: {
     async fetchLogs() {
-      await axios.post("https://u2wfyxf9de.execute-api.eu-central-1.amazonaws.com/development/logs").then((response) => {
+      await axios.post("https://r5zvwg1vrb.execute-api.eu-central-1.amazonaws.com/development/logs").then((response) => {
         const data = response.data;
         const body = JSON.parse(data.body);
         this.$store.commit('setLogs', body);
