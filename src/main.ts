4eb553b2d2f0e6c7876c2092f59182996b443c87
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-03-10 23:11:17 +0200

    Remove cors

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,10 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import router from './router'
 import store from './store/index'
-import cors from 'cors'
 
 const app = createApp(App)
 
 app.use(router);
 app.use(store);
-app.use(cors);
 
 app.mount('#app')
